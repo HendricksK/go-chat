b7181ee16949b06bfdb16f942b2eba741680f797
HendricksK <kurvinhendricks@gmail.com>
2020-05-31 22:14:39 +0200

    working on backend now, adding a new package

--- backend/main.go
+++ backend/main.go
@@ -3,12 +3,26 @@ package main
 import (
     "fmt"
     "net/http"
+    "https://github.com/HendricksK/go-chatapp/pkg/websocket"
 )
+
+func serveWs(w http.ResponseWriter, r *http.Request) {
+    ws, err := websocket.Upgrade(w, r)
+    if err != nil {
+        fmt.Fprint(w, "%+V\n", err)
+    }
+
+    go websocket.Writer(ws)
+    websocket.Reader(ws)
+}
+
+func setupRoutes() {
+    http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
 
     fmt.Println("chat app v0.0.1")
-
 	setupRoutes()
     http.ListenAndServe(":8080", nil)
     
